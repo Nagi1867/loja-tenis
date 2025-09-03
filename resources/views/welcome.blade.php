e6de2698a97a21661184a66fccdd1e19ad4c0fa7
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-09-03 08:24:37 -0300

    ajustando espaçamento no mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,10 @@
         <div class="flex flex-row gap-5">
             <ul class="flex gap-4">
                 <li class="border-r-2 px-2 py-2 hover:bg-black hover:text-cyan-500"><a href="#">Nova tees</a></li>
-                <li class="border-r-2 px-2 py-2 hover:bg-black hover:text-cyan-500"><a href="#">Tees pra elas</a></li>
-                <li class="border-r-2 px-2 py-2 hover:bg-black hover:text-cyan-500"><a href="#">Tees pra eles</a></li>
+                <li class="border-r-2 px-2 py-2 hover:bg-black hover:text-cyan-500"><a href="#">Tees pra elas</a>
+                </li>
+                <li class="border-r-2 px-2 py-2 hover:bg-black hover:text-cyan-500"><a href="#">Tees pra eles</a>
+                </li>
             </ul>
 
             <ul class="flex gap-4">
@@ -34,45 +36,48 @@
         </div>
     </nav>
 
-    @yield("content")
+    @yield('content')
 
-    <footer class="fixed bottom-0 left-0 w-full border">
-        <div class="w-full flex flex-row items-center">
-            <ul class="bg-black text-white py-20 px-46">
-                <li class="font-bold text-2xl">T SHOP</li>
-                <li>tshop@gmail.com</li>
-                <li>Tel: (11) 3456-7890</li>
+    <footer class="fixed bottom-0 left-0 w-full">
+        <div class="w-full flex flex-col border md:flex-row items-start md:items-center">
+            <ul class="w-full text-center bg-black text-white py-6 px-6 md:py-20 md:px-10">
+                <li class="font-bold text-xl md:text-2xl">T SHOP</li>
+                <li class="text-sm md:text-base">tshop@gmail.com</li>
+                <li class="text-sm md:text-base">Tel: (11) 3456-7890</li>
             </ul>
 
-            <div class="w-full flex items-center ml-auto mx-auto px-60 py-2 gap-25">
-                <ul class="w-full">
-                    <li class="mb-1">Comprar</li>
+            <div
+                class="w-full flex flex-col md:flex-row items-center justify-center text-center px-6 md:px-20 py-4 gap-6 md:gap-20">
+                <ul class="w-full md:w-auto">
+                    <li class="mb-1 font-semibold">Comprar</li>
                     <li class="text-sm">Novo</li>
                     <li class="text-sm">Tees pra elas</li>
                     <li class="text-sm">Tees pra eles</li>
                 </ul>
 
-                <ul class="w-full">
-                    <li class="mb-1">Nossa loja</li>
+                <ul class="w-full md:w-auto">
+                    <li class="mb-1 font-semibold">Nossa loja</li>
                     <li class="text-sm">Sobre nós</li>
                     <li class="text-sm">Assinar</li>
                     <li class="text-sm">FAQ</li>
                 </ul>
 
-                <ul class="w-full">
-                    <li class="mb-1">Termos e Condições</li>
+                <ul class="w-full md:w-auto">
+                    <li class="mb-1 font-semibold">Termos e Condições</li>
                     <li class="text-sm">Política da loja</li>
                     <li class="text-sm">Envio e devolução</li>
                     <li class="text-sm">Métodos de pagamento</li>
                     <li class="text-sm">Políticas de Cookies</li>
                 </ul>
             </div>
+
         </div>
 
-        <div class="w-full text-center py-4 bg-cyan-500 text-white">
+        <div class="w-full text-center py-3 md:py-4 bg-cyan-500 text-white text-sm md:text-base">
             <h1>&copy; 2035 por T Shop. Todos os direitos reservados</h1>
         </div>
     </footer>
+
 </body>
 
 </html>
