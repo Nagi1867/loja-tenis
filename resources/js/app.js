dddbbe0fa2d6ee7f1e07e0b789f6d383ce073bf2
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-09-11 20:59:13 -0300

    carrinho criado

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+const car = document.getElementById('carrinho');
+
+window.carrinho = function() {
+    car.classList.remove('hidden')
+}
+
+window.fecharCarrinho = function () {
+    car.classList.add('hidden')
+}
--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -17,7 +17,25 @@
 
         <ul class="flex gap-4">
             <li class="px-2 py-1 hover:text-blue-300"><a href="#"><i class="fas fa-user"></i> Login</a></li>
-            <li class="px-2 py-1"><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+            <li class="px-2 py-1 hover:text-blue-300" onmouseover="carrinho()" onmouseout="fecharCarrinho()"><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
         </ul>
+
+        <div id="carrinho" class="hidden fixed right-5 top-16 w-69 bg-white shadow-lg border rounded-lg z-[9999]" onmouseover="carrinho()" onmouseout="fecharCarrinho()">
+            <div class="border-b-2 py-3 bg-cyan-500 text-white px-4">
+                <h1>Carrinho (1 item)</h1>
+            </div>
+            <div class="p-4 flex flex-row items-center">
+                <img src="{{ Vite::asset('resources/img/camiseta.jpg') }}" alt="Produto" class="w-30 mr-5">
+                <div>
+                    <p>Camiseta</p>
+                    <p>R$ 15,00</p>
+                    <input type="number" class="border border-b border-cyan-500 p-1 w-10 text-black" min="0"
+                        value="1">
+                    <button
+                        class="bg-cyan-500 text-white px-3 py-1 rounded-2xl mt-3 hover:bg-cyan-950">Carrinho</button>
+                </div>
+            </div>
+        </div>
+
     </div>
 </nav>
